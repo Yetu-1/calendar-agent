eb399073e04a5a6cf2a1ff9d9679b1095c7e42eb
David-Salihu <davidsalihu19@gmail.com>
2025-11-13 18:05:48 +0100

    add routes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from src.agents.ai_agent import CalendarAssistantAgent
 from autogen_core import AgentId, SingleThreadedAgentRuntime
-from autogen_core.models import SystemMessage
 from src.tools.calendar_tools import (
     get_datetime_tool,
     add_event_to_calendar_tool,
@@ -12,9 +11,9 @@ from src.tools.calendar_tools import (
 from typing import List
 from autogen_core.tools import Tool
 from src.config import Settings
-from src.tools.messages import Message
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from src.routes import router, runtime
 
 tools: List[Tool] = [
     get_datetime_tool,
@@ -24,16 +23,11 @@ tools: List[Tool] = [
     delete_event_tool
 ]
 
-app = FastAPI()
-# Create a runtime.
-runtime = SingleThreadedAgentRuntime()
 # Create the model client.
 model_client = OpenAIChatCompletionClient(
     model="gpt-4o-mini",
     api_key=Settings.OPENAI_API_KEY,
 )
-
-calendar_assistant_agent = AgentId("calendar_assistant_agent", "default")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -55,36 +49,4 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-manager = ConnectionManager()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Server Running"}
-
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await manager.connect(websocket)
-    try:
-        while True:
-            message =  Message(client_id=client_id, content=await websocket.receive_text())
-            # Send the message to the calendar assistant agent.
-            response = await runtime.send_message(message, calendar_assistant_agent)
-            await manager.send_message(f"Assistant: {response.content}", websocket)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
+app.include_router(router)
---
+++ src/routes.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, FastAPI, WebSocket, WebSocketDisconnect
+from autogen_core import AgentId, SingleThreadedAgentRuntime
+from src.tools.messages import Message
+
+# Create a runtime.
+runtime = SingleThreadedAgentRuntime()
+calendar_assistant_agent = AgentId("calendar_assistant_agent", "default") # define calendar agent ID
+
+# Websockets connection manager
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+manager = ConnectionManager()
+
+router = APIRouter()
+
+@router.get("/")
+async def root():
+    return {"message": "Server Running"}
+
+@router.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            message =  Message(client_id=client_id, content=await websocket.receive_text())
+            # Send the message to the calendar assistant agent.
+            response = await runtime.send_message(message, calendar_assistant_agent)
+            await manager.send_message(f"Assistant: {response.content}", websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
